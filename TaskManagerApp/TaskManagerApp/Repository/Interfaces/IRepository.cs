ebeb81593921fad77b43d9ae9036944cd007e68f
suporte <suporte@STFBHZC044043-L>
2021-03-14 16:00:09 -0300

    Criação de interface que contém métodos comum entre os repositórios.

---
+++ TaskManagerApp/TaskManagerApp/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaskManagerApp.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        string Serialize(List<T> array);
+        Task PersistirAsync(string data);
+        Guid GetNewGuid();
+    }
+}
