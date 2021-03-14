bfd63d2bf3a8e2cf8e3a341a30ae6adff0a122d5
suporte <suporte@STFBHZC044043-L>
2021-03-14 14:47:27 -0300

    Criado service do Login para realização do login do usuário.

---
+++ TaskManagerApp/TaskManagerApp/Service/Interfaces/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using TaskManagerApp.Domain;
+
+namespace TaskManagerApp.Service.Interfaces
+{
+    public interface ILoginService
+    {
+        Task<Guid> LoginAsync(Usuario usuario);
+        void ValidarDadosLogin(Usuario usuario);
+    }
+}
---
+++ TaskManagerApp/TaskManagerApp/Service/LoginService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using TaskManagerApp.Domain;
+using TaskManagerApp.Repository.Interfaces;
+using TaskManagerApp.Service.Interfaces;
+using TaskManagerApp.Utils.Exceptions;
+
+namespace TaskManagerApp.Service
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public LoginService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task<Guid> LoginAsync(Usuario usuario)
+        {
+            ValidarDadosLogin(usuario);
+            return await _usuarioRepository.Login(usuario);
+        }
+
+        public void ValidarDadosLogin(Usuario usuario)
+        {
+            if(string.IsNullOrEmpty(usuario.Login))
+            {
+                throw new RequiredFieldException("Por favor, informe o Login do usuário.");
+            }
+
+            if (string.IsNullOrEmpty(usuario.Password))
+            {
+                throw new RequiredFieldException("Por favor, informe a Senha do usuário.");
+            }
+        }
+    }
+}
