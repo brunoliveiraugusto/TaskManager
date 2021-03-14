b20e4722f34e861a8a1d07b0ee76b508e6f4c976
suporte <suporte@STFBHZC044043-L>
2021-03-14 13:55:19 -0300

    Criada sobrescrita do método OnStartup e injetado dependências no container de injeção de dependências.

--- TaskManagerApp/TaskManagerApp/App.xaml.cs
+++ TaskManagerApp/TaskManagerApp/App.xaml.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TaskManagerApp.Repository;
+using TaskManagerApp.Repository.Interfaces;
+using TaskManagerApp.Service;
+using TaskManagerApp.Service.Interfaces;
 
 namespace TaskManagerApp
 {
@@ -13,5 +21,30 @@ namespace TaskManagerApp
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IUsuarioService, UsuarioService>();
+
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IFileRepository, FileRepository>();
+            services.AddSingleton<IFileSystem>(new FileSystem());
+
+            services.AddTransient(typeof(MainWindow));
+        }
     }
 }
