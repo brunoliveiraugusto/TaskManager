fad10eebd4368afeca70d7aa0b6fe7fcadb3345d
suporte <suporte@STFBHZC044043-L>
2021-03-14 14:48:11 -0300

    Criada exceção de usuário não encontrado.

---
+++ TaskManagerApp/TaskManagerApp/Utils/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskManagerApp.Utils.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base("Usuário ou senha invalido.")
+        {
+
+        }
+    }
+}
