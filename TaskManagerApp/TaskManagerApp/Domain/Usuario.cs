6c072df9e459bcbed7c16673f3199cfacd064347
suporte <suporte@STFBHZC044043-L>
2021-03-14 13:55:39 -0300

    Criado modelo Usuario.

---
+++ TaskManagerApp/TaskManagerApp/Domain/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagerApp.Domain
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
