9bb6482a2220482016bf15e6592833e4c2d9f368
suporte <suporte@STFBHZC044043-L>
2021-03-14 16:00:22 -0300

    Criada entidade Tarefa.

---
+++ TaskManagerApp/TaskManagerApp/Domain/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TaskManagerApp.Domain
+{
+    public class Tarefa
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataEdicao { get; set; }
+        public Guid IdUsuario { get; set; }
+    }
+}
