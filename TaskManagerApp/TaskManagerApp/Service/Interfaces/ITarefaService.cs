a9a2f584c1b6654f8e88f60d788ef7fe9744a701
suporte <suporte@STFBHZC044043-L>
2021-03-16 19:30:13 -0300

    Criação de serviço da tarefa e métodos para validar dados de cadastro e criação da tarefa.

---
+++ TaskManagerApp/TaskManagerApp/Service/Interfaces/ITarefaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagerApp.Domain;
+
+namespace TaskManagerApp.Service.Interfaces
+{
+    public interface ITarefaService
+    {
+        Task CadastarTarefaAsync(Tarefa tarefa);
+        void ValidarDadosPreenchidosCadastroTarefa(Tarefa tarefa);
+    }
+}
---
+++ TaskManagerApp/TaskManagerApp/Service/TarefaService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagerApp.Domain;
+using TaskManagerApp.Repository.Interfaces;
+using TaskManagerApp.Service.Interfaces;
+using TaskManagerApp.Utils.Exceptions;
+
+namespace TaskManagerApp.Service
+{
+    public class TarefaService : ITarefaService
+    {
+        private readonly ITarefaRepository _tarefaRepository;
+
+        public TarefaService(ITarefaRepository tarefaRepository)
+        {
+            _tarefaRepository = tarefaRepository;
+        }
+
+        public async Task CadastarTarefaAsync(Tarefa tarefa)
+        {
+            ValidarDadosPreenchidosCadastroTarefa(tarefa);
+            await _tarefaRepository.CriarAsync(tarefa);
+        }
+
+        public void ValidarDadosPreenchidosCadastroTarefa(Tarefa tarefa)
+        {
+            if(string.IsNullOrEmpty(tarefa.Descricao))
+            {
+                throw new RequiredFieldException("Por favor, informe a Descrição da tarefa.");
+            }
+        }
+    }
+}
