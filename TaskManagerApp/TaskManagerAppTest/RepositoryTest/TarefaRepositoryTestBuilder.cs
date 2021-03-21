8289b050356e8d859aaa3a25539a9fa1f8b12523
suporte <suporte@STFBHZC044043-L>
2021-03-21 13:41:23 -0300

    Criação de builder para criar um mock da interface utilizada pela tarefa.

---
+++ TaskManagerApp/TaskManagerAppTest/RepositoryTest/TarefaRepositoryTestBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using TaskManagerApp.Repository.Interfaces;
+
+namespace TaskManagerAppTest.RepositoryTest
+{
+    public class TarefaRepositoryTestBuilder
+    {
+        protected Mock<ITarefaRepository> Model;
+
+        public TarefaRepositoryTestBuilder()
+        {
+            Model = new Mock<ITarefaRepository>();
+        }
+
+        public Mock<ITarefaRepository> Build()
+        {
+            return Model;
+        }
+    }
+}
