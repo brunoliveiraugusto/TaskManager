21611e0cd5395f37615d6c890a0e9734c1e6c307
suporte <suporte@STFBHZC044043-L>
2021-03-21 12:04:04 -0300

    Criação da classe Settings para representar a área Settings do arquivo appsettings.

---
+++ TaskManagerApp/TaskManagerApp/Configuration/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManagerApp.Configuration
+{
+    public class Settings
+    {
+        public string SourcePath { get; set; }
+        public string ExtensionFile { get; set; }
+    }
+}
