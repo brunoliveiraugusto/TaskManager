26e4e45e2f56d8ce631561759b78e4f75ffdb409
suporte <suporte@STFBHZC044043-L>
2021-03-21 13:40:33 -0300

    Criação de builder para criar um objeto default do tipo Tarefa.

---
+++ TaskManagerApp/TaskManagerAppTest/DomainTest/TarefaTestBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using System;
+using TaskManagerApp.Domain;
+
+namespace TaskManagerAppTest.DomainTest
+{
+    public class TarefaTestBuilder
+    {
+        protected Tarefa Model;
+
+        public TarefaTestBuilder()
+        {
+            Model = new Tarefa();
+        }
+
+        public TarefaTestBuilder Default()
+        {
+            Model.DataCriacao = DateTime.Now;
+            Model.Descricao = new Faker().Lorem.Words(5).ToString();
+            Model.Id = Guid.NewGuid();
+            Model.IdUsuario = Guid.NewGuid();
+            Model.TarefaConcluida = false;
+
+            return this;
+        }
+
+        public Tarefa Build()
+        {
+            return Model;
+        }
+    }
+}
